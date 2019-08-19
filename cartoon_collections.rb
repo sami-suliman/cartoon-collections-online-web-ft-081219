af5f702a9827c4baf0d88ed8f827e868cc602bda
sami suliman <sami.bco@gmail.com>
2019-08-19 02:29:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,5 @@ end
 def find_the_cheese(array) # code an argument here
   # the array below is here to help
   #cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find?{|i| i}
-  
+  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
